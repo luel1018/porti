6f12233d602783b1d2a990d7873584e9021ccf8e
Tahcy-99 <sjae999@gmail.com>
2026-02-27 14:47:01 +0900

    [Feat] 개인 사용자 로그인 구현 1
    Dto에 핸드폰 번호 추가

--- src/main/java/org/example/porti/user/model/UserDto.java
+++ src/main/java/org/example/porti/user/model/UserDto.java
@@ -10,6 +10,7 @@ public class UserDto {
     public static class SignupReq {
         private String email;
         private String name;
+        private String phone;
         private String password;
 
         public User toEntity() {
@@ -17,6 +18,7 @@ public class UserDto {
                     .email(this.email)
                     .name(this.name)
                     .password(this.password)
+                    .phone(this.phone)
                     .enable(false)
                     .role("ROLE_USER")
                     .build();
