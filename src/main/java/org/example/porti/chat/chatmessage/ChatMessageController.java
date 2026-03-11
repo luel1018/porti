c887d0b9595b5cc8ae3b77047937a7d3723e14ee
swoo <kokodd1234@gmail.com>
2026-03-11 20:54:21 +0900

    [Feat] Websocket을 통한 실시간 채팅 메세지 송신/수신 기능 구현 완료

--- src/main/java/org/example/porti/chat/chatmessage/ChatMessageController.java
+++ src/main/java/org/example/porti/chat/chatmessage/ChatMessageController.java
@@ -1,9 +1,29 @@
 package org.example.porti.chat.chatmessage;
 
 import lombok.RequiredArgsConstructor;
+import org.example.porti.chat.chatmessage.model.ChatMessageDto;
+import org.example.porti.user.model.AuthUserDetails;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatMessageController {
+    private final ChatMessageService chatMessageService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat/message")
+    public void sendMesage(ChatMessageDto.Send req, StompHeaderAccessor accessor) {
+        Authentication authentication = (Authentication) accessor.getSessionAttributes().get("user");
+        AuthUserDetails user = (AuthUserDetails) authentication.getPrincipal();
+
+        ChatMessageDto.Res res = chatMessageService.saveMessage(req, user.getIdx());
+
+        messagingTemplate.convertAndSend("/sub/chat/room/" + req.getRoomIdx(), res);
+    }
+
+
 }
--- src/main/java/org/example/porti/config/WebsocketConfig.java
+++ src/main/java/org/example/porti/config/WebsocketConfig.java
@@ -6,7 +6,6 @@ import org.example.porti.config.interceptor.JwtHandshakeInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -22,8 +21,7 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .addInterceptors(jwtHandshakeInterceptor)
-                .setAllowedOrigins("*")
-                .withSockJS();
+                .setAllowedOrigins("*");
     }
 
     @Override
